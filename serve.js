796129cf692769d1103c65103fe43c99a1b30b19
Aluno.Etec <aula@etec.com>
2026-03-24 16:39:03 -0300

    atualização

--- serve.js
+++ serve.js
@@ -7,7 +7,36 @@ const app = express(); ///vai trazer o retorno da execução da função
 const porta = 3000;
 
 app.get("/",(request, response) => {
-    response.send("Olá, eu vou tirar mb nessa matéria!")
+    response.send("Olá, mundo!")
+});
+
+app.get("/usuario", (request, response) => {
+    response.json([
+        {
+        id: 1,
+        nome: "Ana Vitória",
+        profissao:"aluna de panquequinha de banãnã"
+        },
+
+        {
+        id: 2,
+        nome: "Paulo",
+        profissao:"menino do i"
+        },
+
+        {
+        id: 3,
+        nome: "Larissa",
+        profissao:"menina risonha"
+        },
+
+        {
+        id: 4,
+        nome: "Antônia",
+        profissao:"Carolzinha Conká"
+        },
+
+    ]);
 });
 
 app.listen(porta, (resquest, response) => {
